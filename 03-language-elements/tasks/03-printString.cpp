c2d36eb23d098d3ce389a96638293d23cb496fb8
automationdream <automationdream@gmail.com>
2022-09-22 19:48:27 +0200

    my implementation

--- 03-language-elements/tasks/03-printString.cpp
+++ 03-language-elements/tasks/03-printString.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-// Write your function here
+void printString(const std::string &str, int rep) {
+    if (rep < 0) {
+        std::cout << "Function accepts only positive values." << "\n";
+    } else {
+        for (size_t i = 0; i < rep; i++) {
+            std::cout << str << "\n";
+        };
+    }
+
+};
 
 int main() {
     printString("Hello", 5);
@@ -10,6 +19,9 @@ int main() {
     std::cout << "\n";
 
     printString("HiHi ", 6);
+    std::cout << "\n";
+
+    printString("HiHi ", -1);
     std::cout << "\n";
 
     return 0;
