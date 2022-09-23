5129c034427692cef835a887ef3bc89c96c60a36
automationdream <automationdream@gmail.com>
2022-09-23 10:32:22 +0200

    my implementation

--- 04-stl-basics/tasks/01-printVector.cpp
+++ 04-stl-basics/tasks/01-printVector.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 #include <string>
 
-// Implement printVector to print the content of a given vector on screen.
-// Each string in a new line.
+void printVector(const std::vector<std::string> &vec){
+    for (auto element:vec){
+        std::cout << element << '\n';
+    };
+}
 
 int main() {
     std::vector<std::string> vec {
