ed346dba151f8d200a098b74ddb62920829284ed
automationdream <automationdream@gmail.com>
2022-09-23 15:49:34 +0200

    my implementation

--- 04-stl-basics/tasks/03-createSortedList.cpp
+++ 04-stl-basics/tasks/03-createSortedList.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
-// Implement createSortedList
-// add proper include :)
+std::list<int> createSortedList(const std::vector<int> &vec) {
+    std::list<int> lst;
+
+    for (const auto element:vec) {
+        lst.push_back(element);
+    }
+
+    lst.sort();
+
+    return lst;
+}
 
 int main() {
     std::vector<int> vec{2, 3, 4, 1, 6, 5, 8, 7, 9, 0};
